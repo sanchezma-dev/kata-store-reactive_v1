2737f20c22490a75ae1314fe81c70042d1b203f5
Josema <sanchezma.dev@gmail.com>
2023-12-13 18:02:25 +0100

    aplicacion buscador

---
+++ searchApp/src/main/java/reactive/search/app/searchApp/SearchAppApplication.java
@@ -0,0 +1,13 @@
+package reactive.search.app.searchApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchAppApplication.class, args);
+	}
+
+}
