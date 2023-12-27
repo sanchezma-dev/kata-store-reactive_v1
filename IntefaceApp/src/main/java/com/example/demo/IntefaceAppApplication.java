4f396a9ebde56acbf4db3dce515c95501c134a91
Josema <sanchezma.dev@gmail.com>
2023-12-27 22:58:38 +0100

    Interface app

---
+++ IntefaceApp/src/main/java/com/example/demo/IntefaceAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntefaceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntefaceAppApplication.class, args);
+	}
+
+}
